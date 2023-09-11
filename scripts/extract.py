d1069cf0a79502a49139a893950939ebd83e3825
Brandon Ros <brandonros1@gmail.com>
2023-09-10 20:13:41 -0400

    migrate extract to python for performance

---
+++ scripts/extract.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import sys
+import json
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: extract.py <OUTCOME_ID>")
+        sys.exit(1)
+
+    outcome_id = sys.argv[1]
+    output_filename = f"./generated/{outcome_id}.csv"
+
+    with open("output.tsv", "r") as infile, open(output_filename, "w") as outfile:
+        # Write the header to the CSV
+        outfile.write("timestamp,implied_probability\n")
+
+        # Iterate through each line of the input
+        for line in infile:
+            parts = line.strip().split('\t')
+            # Check if the current line matches our conditions
+            if "PriceEvent" in line and outcome_id in line:
+                try:
+                    timestamp = parts[1]
+                    # Try to extract the decimal price from the JSON string
+                    event = json.loads(parts[3])
+                    decimal_price = float(event["price"]["decimal"])
+                    implied_probability = round(1 / decimal_price, 2)
+                    outfile.write(f"{timestamp},{implied_probability}\n")
+                except (IndexError, ValueError, KeyError, TypeError) as e:
+                    # Handling potential JSON parsing errors, or missing keys
+                    continue
+
+if __name__ == "__main__":
+    main()
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    ./scripts/extract.sh "1422096928" && ./scripts/plot.sh "1422096928" # Daniil Medvedev
-    ./scripts/extract.sh "1422096929" && ./scripts/plot.sh "1422096929" # Novak Djokovic
+    ./scripts/extract.py "1422096928" && ./scripts/plot.sh "1422096928" # Daniil Medvedev
+    ./scripts/extract.py "1422096929" && ./scripts/plot.sh "1422096929" # Novak Djokovic
     sleep 5
 done
